4b8b964402c136a71ff3cf29a0c2a80c8a1e4ace
mirror-shades <44949816+mirror-shades@users.noreply.github.com>
2024-02-26 21:07:44 -0800

    typo

--- src/components/Chatbot.svelte
+++ src/components/Chatbot.svelte
@@ -141,7 +141,7 @@
   <select class="bg-base-100" bind:value={model}>
     <option value="gpt-4-0125-preview">GPT-4 Turbo</option>
     <option value="gpt-4">GPT-4</option>
-    <option value="gpt-3.5-turbo">GPT-3.5</option>
+    <option value="gpt-3.5-turbo">GPT-3.5 Turbo</option>
   </select>
 </label>
 <br />
